199fd23fa4597b4273a7aa987aa9e4485a585d7c
ItFogOfPast <65763631+ItFogOfPast@users.noreply.github.com>
2023-04-13 15:28:19 +0300

    Added new files

---
+++ Controllers/SendToController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Security;
+using System.Linq;
+using PlazmaBurstMMO.Models;
+
+namespace PlazmaBurstMMO.Controllers
+{
+    [Route("api/GetMoney")]
+    [ApiController]
+    public class SendToController : ControllerBase
+    {
+        public IActionResult GetMoneyResponse([FromHeader] PBData pb2data) => Ok("Awesome! I love you mister " + pb2data.user_login);
+    }
+
+}
---
+++ Models/PBData.cs
@@ -0,0 +1,11 @@
+﻿namespace PlazmaBurstMMO.Models
+{
+    public class PBData
+    {
+        public string map_id { get; set; }
+
+        public string user_uid { get; set; }
+
+        public string user_login { get; set; }
+    }
+}
